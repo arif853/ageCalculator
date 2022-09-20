672d6e4af4309ba5c75d6e9270120e011a9827ed
arif853 <arifhossen853@gmail.com>
2022-09-20 21:10:00 +0600

    ageCalculator

--- js/script.js
+++ js/script.js
@@ -4,11 +4,33 @@ function birthday(){
 
     var d = new Date(birthday);
     var p = new Date()
-    var year = p.getFullYear() - d.getFullYear()
-    var month = Math.abs(p.getMonth() - d.getMonth())
-    var day = Math.abs(p.getDate() - d.getDate())
     
-    document.getElementById("year").textContent ="Your age is " + year + " years"
-    document.getElementById("month").textContent = month + " month"
-    document.getElementById("day").textContent = day + " days"
+    d1 = d.getDate()
+    m1 = d.getMonth()
+    y1 = d.getFullYear()
+
+    d2 = p.getDate()
+    m2 = p.getMonth()
+    y2 = p.getFullYear()
+
+    var m = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if(d1>d2){
+        d2 = d2 + m[m2-1]
+        m2 = m2 - 1
+        
+    }
+    if(m1>m2){
+        m2 = m2 + 12
+        y2 = y2 - 1
+    }
+    console.log(d2)
+    console.log(m2)
+    day = d2 - d1
+    month = m2 - m1
+    year = y2 - y1
+    
+    document.getElementById("year").innerHTML ="Your age is " + year + " years"
+    document.getElementById("month").innerHTML = month + " month"
+    document.getElementById("day").innerHTML = day + " days"
 }
